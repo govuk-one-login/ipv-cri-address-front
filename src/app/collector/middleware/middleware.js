789b141b704dd39805f5a1a58e18f384d5bf3136
Matthew Bidewell <matthewbidewell@gmail.com>
2022-01-19 21:31:50 +0000

    Add address collector middleware

---
+++ src/app/collector/middleware/middleware.js
@@ -0,0 +1,16 @@
+// const axios = require("axios");
+// const {
+//   API: {
+//     BASE_URL,
+//     PATHS: { AUTHORIZE },
+//   },
+// } = require("../../lib/config");
+
+module.exports = {
+  addAddressToSession: async (req, res, next) => {
+    // const address = req.body;
+    // console.log("\nposties");
+    // console.log(address);
+    next();
+  },
+};
