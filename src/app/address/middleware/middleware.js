f797bfcb31bcacc301aef4592aa0853a51598e08
Matthew Bidewell <matthewbidewell@gmail.com>
2022-01-21 11:07:01 +0000

    Delete unused files

--- src/app/address/middleware/middleware.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  addAddressToSession: async (req, res, next) => {
-    // const address = req.body;
-    console.log("\nposties");
-    // console.log(address);
-    next();
-  },
-};
