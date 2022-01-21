211bb2c7935d2cbeac5aef82ad30b7bc56b4ff78
Matthew Bidewell <matthewbidewell@gmail.com>
2022-01-21 11:06:44 +0000

    Removed unused fields

--- src/app/address/index.js
+++ src/app/address/index.js
@@ -9,13 +9,6 @@ const router = express.Router();
 router.use(
   require("hmpo-form-wizard")(steps, fields, {
     name: "address",
-    fields: [
-      "address-line-1",
-      "address-line-2",
-      "address-town",
-      "address-county",
-      "address-postcode",
-    ],
     journeyName: "address",
     templatePath: "address",
   })
--- src/app/address/steps.js
+++ src/app/address/steps.js
@@ -8,13 +8,6 @@ module.exports = {
     next: "address",
   },
   "/address": {
-    fields: [
-      "address-line-1",
-      "address-line-2",
-      "address-town",
-      "address-county",
-      "address-postcode",
-    ],
     controller: address,
     next: "/",
   },
