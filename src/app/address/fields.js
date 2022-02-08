50c189813433ea549c933858365ea135f35c4308
Matthew Bidewell <matthewbidewell@gmail.com>
2022-02-08 16:14:31 +0000

    Update address form view

--- src/app/address/fields.js
+++ src/app/address/fields.js
@@ -1,15 +1,23 @@
 module.exports = {
   "address-line-1": {
     type: "text",
+    autocomplete: "address-line1",
   },
   "address-line-2": {
     type: "text",
+    autocomplete: "address-line2",
   },
   "address-town": {
     type: "text",
+    autocomplete: "address-line1",
+  },
+  "address-postcode": {
+    type: "text",
+    autocomplete: "Postcode",
   },
   "address-county": {
     type: "text",
+    autocomplete: "address-level2",
   },
   "address-search": {
     type: "text",
