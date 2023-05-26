dec7002ffc1fc9ee70857c90f7860a540f19e755
Robin Harrison <robin.harrison@digital.cabinet-office.gov.uk>
2023-05-26 17:24:31 +0100

    feat: Use tags as client_id

--- test/browser/support/setup.js
+++ test/browser/support/setup.js
@@ -6,7 +6,6 @@ const {
   setDefaultTimeout,
 } = require("@cucumber/cucumber");
 const { chromium } = require("playwright");
-const axios = require("axios");
 
 // FIXME This is large due to cold starts
 setDefaultTimeout(30 * 1000);
@@ -42,30 +41,12 @@ Before(async function ({ pickle } = {}) {
 
   const header = tag?.name.substring(10);
 
-  this.SCENARIO_ID_HEADER = header;
-
-  try {
-    await axios.get(`${process.env.API_BASE_URL}/__reset/${header}`);
-  } catch (e) {
-    /* eslint-disable no-console */
-    console.log("Error resetting mock");
-    console.log(`${process.env.API_BASE_URL}/__reset/${header}`);
-    console.log(e.message);
-    /* eslint-enable no-console */
-    throw e;
-  }
+  this.TESTING_CLIENT_ID = header;
 });
 
 // Create a new test context and page per scenario
 Before(async function () {
   this.context = await global.browser.newContext({});
-
-  if (this.SCENARIO_ID_HEADER) {
-    await this.context.setExtraHTTPHeaders({
-      "x-scenario-id": this.SCENARIO_ID_HEADER,
-    });
-  }
-
   this.page = await this.context.newPage();
 });
 
