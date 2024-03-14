6ca5af13c6b4158a2f945a915f085d7b5c08c041
di-aholme <andy.holme@digital.cabinet-office.gov.uk>
2024-03-14 16:40:27 +0000

    [FEC-GA4] - Remove TODOs flagged by Sonar

--- src/lib/config.js
+++ src/lib/config.js
@@ -19,9 +19,9 @@ module.exports = {
     GTM: {
       ANALYTICS_COOKIE_DOMAIN:
         process.env.ANALYTICS_COOKIE_DOMAIN || "localhost",
-      UA_DISABLED: process.env.UA_DISABLED || "false", // TODO: Change to 'true' after data analyst QA
+      UA_DISABLED: process.env.UA_DISABLED || "false",
       UA_CONTAINER_ID: process.env.UA_CONTAINER_ID,
-      GA4_DISABLED: process.env.GA4_DISABLED || "true", // TODO: Change to 'false' after data analyst QA
+      GA4_DISABLED: process.env.GA4_DISABLED || "true",
       GA4_CONTAINER_ID: process.env.GA4_CONTAINER_ID,
     },
   },
