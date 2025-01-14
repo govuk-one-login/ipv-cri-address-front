98800987cb9e575fcca76b4a4039ea3a1d0275d4
Suraj Kumar <sk96.uk@gmail.com>
2025-01-14 10:21:52 +0000

    feat: added logging for when there is an error callback

--- src/app/address/controllers/address/whatCountry.js
+++ src/app/address/controllers/address/whatCountry.js
@@ -1,10 +1,14 @@
+const { PACKAGE_NAME } = require("../../../../lib/config");
 const BaseController = require("hmpo-form-wizard").Controller;
+
+const logger = require("hmpo-logger").get(PACKAGE_NAME);
 
 class WhatCountryController extends BaseController {
   async getValues(req, res, callback) {
     super.getValues(req, res, (err, values) => {
       values.country = "";
       if (err) {
+        logger.warn("Calling callback with err: " + err);
         callback(err, values);
       } else {
         callback(null, values);
