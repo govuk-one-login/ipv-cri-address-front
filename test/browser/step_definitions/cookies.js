eea158864d9c1d8ee7601dad90d4670c954b9a17
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2025-04-18 10:46:43 +0100

    Testing

--- test/browser/step_definitions/cookies.js
+++ test/browser/step_definitions/cookies.js
@@ -3,7 +3,21 @@ const { expect } = require("chai");
 
 Then("the {word} cookie has been set", async function (cookieName) {
   await this.page.waitForTimeout(1000);
+
+  const href = await this.page.evaluate(() => {
+    // eslint-disable-next-line no-undef
+    return document.location.href;
+  });
+
+  console.log("🍏 href", href);
+
+  const content = await this.page.content();
+  console.log("🍏 content", content);
+
   const cookies = await this.page.context().cookies();
+
+  console.log("🍏 cookies", cookies);
+
   const expectedCookie = cookies.find((cookie) => cookie.name === cookieName);
   expect(expectedCookie).to.exist;
 });
