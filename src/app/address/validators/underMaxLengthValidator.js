4f979e66aab8a8fd6b8d644c1a55d651731eb7a7
Sam Bunce <samuel.bunce@digital.cabinet-office.gov.uk>
2026-09-24 16:03:02 +0100

    feat(DFC-1288): Migrate from HMPO-components to frontend-ui

--- src/app/address/validators/underMaxLengthValidator.js
+++ src/app/address/validators/underMaxLengthValidator.js
@@ -1,6 +1,6 @@
 /**
-  We need to use a custom max length validator for hmpoText fields.
-  Having a validator type of maxlength causes hmpoText components to set the maxlength HTML attribute.
+  We need to use a custom max length validator for frontend-ui (formerly HMPO) text fields.
+  Having a validator type of maxlength causes text components to set the maxlength HTML attribute.
   This causes accessibility issues.
   */
 export function underMaxLength(val, max) {
